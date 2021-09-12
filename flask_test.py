4d25509e59ee2d39de849d98f19f06b101cbe7a9
Allen Tsai <a0962071938@gmail.com>
2021-09-12 17:12:05 +0800

    修改主程式檔名

---
+++ flask_test.py
@@ -0,0 +1,20 @@
+# 使用heroku要建立runtime.txt，告訴人家我們使用的python是哪一個版本
+# 建立requierments.txt，專案運作時需要使用那些套件，gunicorn我們沒用到，但是要在heroku上啟動專案，必須加入
+# 建立Procfile，告訴heroku我們要如何啟動專案heroku是使用gunicorn啟動我們的web專案，後面加上
+from flask import Flask
+
+app = Flask(__name__)  # __name__ 代表目前執行的模組
+
+
+@app.route("/")  # 函式的裝飾 (Decorator) : 以函式為基礎，提供附加的功能
+def home():
+    return "Hello Flask 2"
+
+
+@app.route("/test")  # 代表我們要處理的網站路徑
+def test():
+    return "test now 222"
+
+
+if __name__ == "__main__":  # 如果以主程式執行
+    app.run()  # 立刻啟動伺服器
